79d86acec49801a485cd10481a9236cb2cea2351
r-e-n-a-t <ren2013ner@gmail.com>
2022-09-15 20:44:39 +1000

    add functions: ShowArray and CreateArray

--- Program.cs
+++ Program.cs
@@ -4,6 +4,34 @@
     string result = Console.ReadLine();
     return result;
 }
-
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+string[] CreateArray()
+{   
+    int count = 0;
+    string value = "";
+    string[] tempArray = new string[1000];
+    System.Console.WriteLine($"Для завершения ввода введите 'exit'");
+    for (count = 0; value != "exit"; count++)
+    {
+        value = Prompt($"Введите значение элемента {count+1}> ");
+        if (value != "exit") 
+        {
+            tempArray[count] = value;
+        }
+    }
+    string[] array = new string[count-1];
+    for (int i = 0; i < array.Length; i++)
+    {
+         array[i] = tempArray[i];
+    }
+    return array;
+}
 
 
